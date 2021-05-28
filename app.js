b420cb6fea197d635c4ae12601de6a60e67a46a5
Hector <h_berru@hotmail.com>
2021-05-28 17:13:34 -0300

    subo todo el proyecto. sin imagenes, sin main y con formularios de registro y login sin estetica

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+
+const path = require ('path');
+
+const app = express ();
+
+const publicPath = path.resolve(__dirname, './public');
+
+
+app.use (express.static(publicPath));
+
+app.listen(3000, ()=> {
+    console.log ('Server on port 3000');
+});
+app.use('/public/images', express.static(__dirname +'/public/images'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+});
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+
+
