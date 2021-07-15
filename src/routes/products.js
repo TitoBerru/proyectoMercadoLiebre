189a8018d39dddd6e7e1465d1cc0ce89420b9d3a
TitoBerru <c_myco@hotmail.com>
2021-07-15 12:47:49 -0300

    add crud complet

---
+++ src/routes/products.js
@@ -0,0 +1,45 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/images/products'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName = 'img' + '-' + file.originalname;   // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+});
+
+const uploadFile = multer({ storage: multerDiskStorage });
+
+
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create', productsController.create); 
+router.post('/create', uploadFile.single('imagenProducto'), productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', productsController.update); 
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/:id', productsController.destroy); 
+
+
+module.exports = router;
